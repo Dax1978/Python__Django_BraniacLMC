3263b3fa233754db99fdd879e38f2490ea6fbe6d
Dax <John-Xenos@yandex.ru>
2022-12-17 18:34:25 +0300

    Выполнено все до начала внесения учебного сайта

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
 
 # Create your views here.
+
+# def hello_world(request):
+#     return HttpResponse("Hello world!")
+class HelloWorldView(View):
+    def get(self, *args):
+        return HttpResponse("Hello world!")
+
+def check_kwargs(request, **kwargs):
+    return HttpResponse(f"kwargs:<br>{kwargs}")
